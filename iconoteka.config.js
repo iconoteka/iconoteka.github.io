4f0f9212ed5ce5d3b132e1cae74c863c9928ac43
Aleksei Chernov <kot.lex@gmail.com>
2020-05-29 23:26:52 +0300

    Update engine to use advanced GA tracking

--- iconoteka.config.js
+++ iconoteka.config.js
@@ -1,8 +1,20 @@
 const path = require('path');
 
+// Workaround to avoid errors on inconoteka-engine postinstall time
+let iconotekaJson = '';
+let iconotekaFilesPath = '';
+
+try {
+    iconotekaJson = path.join(path.dirname(require.resolve('iconoteka')), 'iconoteka.json');
+    iconotekaFilesPath = path.join(path.dirname(require.resolve('iconoteka')), 'iconoteka');
+} catch(e) {
+
+}
+
 const config = {
-    iconotekaJson: path.join(path.dirname(require.resolve('iconoteka')), 'iconoteka.json'),
-    iconotekaFilesPath: path.join(path.dirname(require.resolve('iconoteka')), 'iconoteka')
+    iconotekaJson,
+    iconotekaFilesPath,
+    googleAnalyticsId: 'UA-125050250-1', 
 };
 
 module.exports = config;
